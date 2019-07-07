05af72d1f16b1a717132668dc74997d4e3d51794
anthonypenna <penna1296@gmail.com>
2019-07-07 20:02:02 +0200

    add creation functionality

--- src/lib/createApplication.js
+++ src/lib/createApplication.js
@@ -1,12 +1,35 @@
+const util         = require('util')
+const childProcess = require('child_process')
+
+const print        = require('./print')
+const clear        = require('./clear')
 const getArguments = require('./getArguments')
 const printAndExit = require('./printAndExit')
 
-module.exports = function createApplication() {
+module.exports = async function createApplication() {
   const arguments = getArguments()
+  const projectName = arguments[1] || null
 
-  if (arguments.length === 1) {
+  if (arguments.length === 1)
     printAndExit('Please specify a project name.')
-  } else {
-    const projectName = arguments[1]
+
+  if (projectName) {
+    print(`Creating new Svelte app: ${projectName}...`)
+
+    try {
+      const exec = util.promisify(childProcess.exec)
+      await exec(`npx degit sveltejs/template ${projectName}`)
+
+      clear()
+      printAndExit(
+        `${projectName} successfully created.\r\n`     +
+        'To get up and running:              \r\n\r\n' +
+        `  cd ${projectName}                 \r\n`     +
+        '  npm install                       \r\n'     +
+        '  npm run dev                       \r\n'
+      )
+    } catch (error) {
+      throw new Error(error)
+    }
   }
 }
