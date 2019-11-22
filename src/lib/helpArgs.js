463f9cb7a60f4d6d810e6c8b2a6889a45ab1de39
Anthony Penna <anthony.penna@musement.com>
2019-11-22 16:31:16 +0100

    feat(show help): display list of available options

--- src/lib/helpArgs.js
+++ src/lib/helpArgs.js
@@ -1,8 +1,28 @@
-const { printAndExit } = require("../util");
+const { print, printAndExit } = require("../util");
+
+const options = [
+  {
+    flag: "--help, -h",
+    description: "Display a list of currently supported options."
+  },
+  {
+    flag: "--ssr",
+    description: "Create a server side rendered Sapper application."
+  }
+];
 
 module.exports = function helpArgs(args) {
   if (args.includes("--help") || args.includes("-h")) {
-    printAndExit(`Usage: svelte create <project-name>`);
+    let optionsList = "";
+
+    /* build list of available options */
+    options.forEach(({ flag, description }) => {
+      optionsList += `${flag} | ${description}\r\n`;
+    });
+
+    printAndExit(
+      `Usage: svelte create <project-name>\r\nAvailable options:\r\n${optionsList}`
+    );
   }
   return args;
 };
